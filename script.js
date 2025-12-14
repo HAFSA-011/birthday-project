6fb9fe3e46223d7efebb54777d5d45dddb073cd0
HAFSA-011 <hafsajarid54@gmail.com>
2025-12-14 20:03:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ startAudioText.addEventListener("click", () => {
         for (let i = 0; i < bufferLength; i++) sum += dataArray[i];
 
         let average = sum / bufferLength;
-        return average > 65; // العتبة
+        return average > 55; // العتبة
     }
 
     function blowOutCandle() {
